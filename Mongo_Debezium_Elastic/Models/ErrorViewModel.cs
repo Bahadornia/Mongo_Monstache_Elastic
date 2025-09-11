f0fd777fb15f131ebc99db23d25709bb0f73835b
Babak <Babak@FW-2386>
2025-09-11 11:50:55 +0330

    Add project files.

---
+++ Mongo_Debezium_Elastic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mongo_Debezium_Elastic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
